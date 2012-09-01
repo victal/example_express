8a6862128686663eae6df0d5abd0595829195171
Guilherme Victal <guilhermevictal@gmail.com>
2012-09-01 19:26:36 -0300

    Text sandbox
    Movidos arquivos estáticos para static/*
    Adicionado Jquery para client-side
    Adicionada dependencia nodemon para reload do app
    Funcionando em localhost:3000/sandbox

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-//  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'static')));
 });
 
 app.configure('development', function(){
--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,11 @@ var express = require('express')
 module.exports = function(parent){
   var app = express();
   app.set('views', __dirname + '/../views');
-
+  app.use(express.bodyParser());
+  app.post('/sandbox/:id/run',home.run);
+  app.get('/sandbox/:id',home.sandbox);
+  app.get('/sandbox',function(req, res){res.redirect('/sandbox/1')});
   app.get('/',home.home_view);
-
 
   parent.use(app);
 }
